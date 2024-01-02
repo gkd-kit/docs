971072ff1d9a8139e9eb5baa0623e872b6552f99
lisonge <i@songe.li>
2024-01-02 15:56:27 +0800

    fix: only run in browser

--- .vitepress/utils/apk.ts
+++ .vitepress/utils/apk.ts
@@ -1,13 +1,8 @@
 import QRCode from 'qrcode';
 import { computed, shallowRef, watchEffect } from 'vue';
 
-export const apkUrl = shallowRef(localStorage.getItem('apkUrl') || '');
+export const apkUrl = shallowRef('');
 export const apkName = computed(() => apkUrl.value.split('/').at(-1) || '');
-const versionUrl = 'https://registry.npmmirror.com/@gkd-kit/app/latest/files';
-fetch(versionUrl).then(async (r) => {
-  apkUrl.value = new URL((await r.json()).downloadUrl, r.url).href;
-  localStorage.setItem('apkUrl', apkUrl.value);
-});
 export const apkImgUrl = shallowRef('');
 watchEffect(async () => {
   if (!apkUrl.value) {
@@ -19,3 +14,13 @@ watchEffect(async () => {
     margin: 2,
   });
 });
+
+if (globalThis.window) {
+  // only run in browser
+  apkUrl.value = localStorage.getItem('apkUrl') || '';
+  const versionUrl = 'https://registry.npmmirror.com/@gkd-kit/app/latest/files';
+  fetch(versionUrl).then(async (r) => {
+    apkUrl.value = new URL((await r.json()).downloadUrl, r.url).href;
+    localStorage.setItem('apkUrl', apkUrl.value);
+  });
+}
