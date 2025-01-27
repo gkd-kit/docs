7bf6137b80f6d517144acf619ec74d1afc0b875d
二刺螈 <i@songe.li>
2025-01-27 10:46:01 +0800

    feat: animate-hash-hint

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,11 @@
 import 'uno.css';
-import { useRouter, type Theme } from 'vitepress';
+import { type Router, useRouter, type Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import {
   defineComponent,
   Fragment,
   h,
+  nextTick,
   onMounted,
   shallowRef,
   Teleport,
@@ -30,43 +31,70 @@ const ScrollbarWrapper = defineComponent(() => {
   };
 });
 
+const handleCompatRedirect = async (router: Router) => {
+  // 兼容旧链接/短链重定向
+  const u = location.href.substring(location.origin.length);
+  if (location.pathname.startsWith('/selector/')) {
+    if (location.pathname.at(-1) === '/') {
+      router.go('/guide/selector');
+    } else {
+      router.go(location.pathname.replace('/selector/', '/guide/'));
+    }
+  } else if (location.pathname === '/subscription/') {
+    router.go('/guide/subscription');
+  } else if (location.pathname === '/') {
+    const r = new URLSearchParams(location.search).get('r');
+    if (r === '1') {
+      router.go('/guide/snapshot#how-to-upload');
+    } else if (r === '2') {
+      router.go('/guide/faq#restriction');
+    } else if (r === '3') {
+      router.go('/guide/faq#adb_failed');
+    } else if (r === '4') {
+      location.href = 'https://shizuku.rikka.app';
+      return;
+    } else if (r === '5') {
+      router.go('/guide/subscription');
+    } else if (r === '6') {
+      router.go('/guide/faq#power');
+    } else if (r === '7') {
+      router.go('/guide/faq#exact-activity');
+    } else if (r === '8') {
+      router.go('/guide/faq#forced-tap');
+    } else if (r === '9') {
+      router.go('/guide/faq#work-profile');
+    }
+  } else if (u === '/guide/faq#fail_setting_secure_settings') {
+    location.hash = 'adb_failed';
+  }
+  await nextTick();
+  if (location.hash) {
+    const hashEl = await (async () => {
+      const href = location.href;
+      let i = 0;
+      while (i < 20) {
+        if (href !== location.href) return;
+        const el = document.querySelector<HTMLElement>(location.hash);
+        if (el) {
+          return el;
+        }
+        i++;
+        await new Promise((r) => setTimeout(r, 250));
+      }
+    })();
+    if (hashEl) {
+      const hintCls = 'animate-hash-hint';
+      hashEl.classList.add(hintCls);
+      await new Promise((r) => setTimeout(r, 2000));
+      hashEl.classList.remove(hintCls);
+    }
+  }
+};
+
 const Redirect = defineComponent(() => {
   const router = useRouter();
   onMounted(() => {
-    // 兼容旧链接/短链重定向
-    const u = location.href.substring(location.origin.length);
-    if (location.pathname.startsWith('/selector/')) {
-      if (location.pathname.at(-1) === '/') {
-        router.go('/guide/selector');
-      } else {
-        router.go(location.pathname.replace('/selector/', '/guide/'));
-      }
-    } else if (location.pathname === '/subscription/') {
-      router.go('/guide/subscription');
-    } else if (location.pathname === '/') {
-      const r = new URLSearchParams(location.search).get('r');
-      if (r === '1') {
-        router.go('/guide/snapshot#how-to-upload');
-      } else if (r === '2') {
-        router.go('/guide/faq#restriction');
-      } else if (r === '3') {
-        router.go('/guide/faq#adb_failed');
-      } else if (r === '4') {
-        location.href = 'https://shizuku.rikka.app';
-      } else if (r === '5') {
-        router.go('/guide/subscription');
-      } else if (r === '6') {
-        router.go('/guide/faq#power');
-      } else if (r === '7') {
-        router.go('/guide/faq#exact-activity');
-      } else if (r === '8') {
-        router.go('/guide/faq#forced-tap');
-      }else if (r === '9') {
-        router.go('/guide/faq#work-profile');
-      }
-    } else if (u === '/guide/faq#fail_setting_secure_settings') {
-      location.hash = 'adb_failed';
-    }
+    handleCompatRedirect(router);
   });
   return () => {};
 });
