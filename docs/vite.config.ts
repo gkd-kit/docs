f4c0f8185d6faa254afb4c909390ce8b6a35e726
二刺螈 <i@songe.li>
2025-06-17 15:39:59 +0800

    perf: polyfill

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,7 +1,8 @@
 import unocss from 'unocss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type ESBuildOptions } from 'vite';
 import { mirror } from './.vitepress/plugins';
 import data from 'unplugin-data/vite';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   plugins: [
@@ -10,9 +11,16 @@ export default defineConfig({
     }),
     unocss({ inspector: false }),
     mirror(),
+    legacy({ renderLegacyChunks: false, modernPolyfills: true }),
   ],
   server: {
     host: '127.0.0.1',
     port: 8633,
+  },
+  build: {
+    target: 'chrome70',
+  },
+  esbuild: <ESBuildOptions>{
+    legalComments: 'none',
   },
 });
